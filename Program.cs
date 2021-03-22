afd8305ecbdf825328d7d1a6b6b4eebc71b147e7
yong991122 <yong991122@gmail.com>
2021-03-22 15:23:39 +0900

    -백준 알고리즘 문제풀기
     기본수학 1 - 벌집까지 완료

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Algorithm_Practice
+{
+    class Program
+    {
+        static void Main()
+        {
+            // 문제 - 수학1 -벌집문제
+            // 각 회당 벌집방 수가 얼마나 늘어나는지를 수식화
+            // 가고싶은 방이 속해있는 단계 - 1 만큼이 최소거리
+            
+            int x = int.Parse(Console.ReadLine());
+            double n = Math.Sqrt((x - 0.25) / 3) + 0.5;
+
+            Console.WriteLine(Math.Ceiling(n));
+            Console.ReadKey();
+
+        }
+    }
+}
