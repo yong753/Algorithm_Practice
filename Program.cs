9811a78a4edf899f30722fb0b261807d7d244e26
yong991122 <yong991122@gmail.com>
2021-03-24 13:19:11 +0900

    no message

--- Program.cs
+++ Program.cs
@@ -8,13 +8,14 @@ namespace Algorithm_Practice
         {
             // 문제 - 수학1 -벌집문제
             // 각 회당 벌집방 수가 얼마나 늘어나는지를 수식화
-            // 가고싶은 방이 속해있는 단계 - 1 만큼이 최소거리
+            // 가고싶은 방이 속해있는 단계 만큼이 최소거리
             
             int x = int.Parse(Console.ReadLine());
             double n = Math.Sqrt((x - 0.25) / 3) + 0.5;
 
             Console.WriteLine(Math.Ceiling(n));
-            Console.ReadKey();
+            //Console.ReadKey();
+
 
         }
     }
